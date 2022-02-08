0c28c8d8ac07b3139bf737edb08643edc549493e
MphoKut <90329295+MphoKut@users.noreply.github.com>
2022-02-08 14:22:13 +0200

    Functions Nested Loops

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,31 +1,32 @@
 #include "main.h"
 /**
- * Prints the 9 times table
- *
- * Return void
- */
+* times_table - that prints the 9 times table
+*
+* Return: no return
+*/
 void times_table(void)
 {
-int a, b, c;
-for (a = 0; a <= 9; a++)
-{
-putchar(48);
-for (b = 1; b <= 9; b++)
-{
-c = a * b;
-putchar(44);
-putchar(32);
-if (c <= 9)
-{
-putchar(32);
-putchar(c + 48);
-}
-else
-{
-putchar((c / 10) + 48);
-putchar((c % 10) + 48);
-}
-}
-putchar('\n');
-}
+	int a, b, c;
+
+	for (a = 0; a <= 9; a++)
+	{
+		_putchar(48);
+		for (b = 1; b <= 9; b++)
+		{
+			c = a * b;
+			_putchar(44);
+			_putchar(32);
+			if (c <= 9)
+			{
+				_putchar(32);
+				_putchar(c + 48);
+			}
+			else
+			{
+				_putchar((c / 10) + 48);
+				_putchar((c % 10) + 48);
+			}
+		}
+	_putchar('\n');
+	}
 }
