fadcb9cec737ae051e8de8e7f79915f8dbd056c6
MphoKut <90329295+MphoKut@users.noreply.github.com>
2022-02-09 10:52:03 +0200

    Functions Nested Loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,46 +1,20 @@
 #include "main.h"
+
 /**
-<<<<<<< HEAD
-* main - Print the word "_putchar"
-*
-* Return: Always 0 (Success)
-*/
+ * main -  prints Holberton, followed by a new line
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-<<<<<<< HEAD
-int i;
-char p[] = "_putchar";
-for (i = 0; i < 9; i++)
-{
-_putchar(p[i]);
-}
-_putchar('\n');
-return (0);
-=======
-	int i;
-	char p[] = "_putchar";
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
-	for (i = 0; i < 8; i++)
-=======
-* main - Print the word "Holberton"
-*
-* Return: Always 0 (Success)
-*/
-
-int main(void)
-{
-	int i;
-	char p[] = "Holberton";
-
-	for (i = 0; i < 9; i++)
->>>>>>> 0f9c44f97badc3c2c445bfe3792107f0fe804511
-		{
-		_putchar(p[i]);
-		}
-		_putchar('\n');
 	return (0);
-<<<<<<< HEAD
->>>>>>> f33b32a7ac9391feb25e711bf9085cbc73e3a50d
-=======
->>>>>>> 0f9c44f97badc3c2c445bfe3792107f0fe804511
 }
