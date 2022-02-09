92ef4e63aa3596ceeca7af44ab6ed85edeacb851
MphoKut <90329295+MphoKut@users.noreply.github.com>
2022-02-09 10:55:01 +0200

    Functions Nested Loops

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -22,28 +22,3 @@ void print_spaces(int n);
 void print_zero(void);
 
 #endif
-=======
-#ifndef MAIN_H_
-#define MAIN_H_
-
-int _putchar(char c);
-void print_alphabet(void);
-void print_alphabet_x10(void);
-int _islower(int c);
-int _isalpha(int c);
-int print_sign(int n);
-int _abs(int);
-int print_last_digit(int);
-void jack_bauer(void);
-void times_table(void);
-void pr_digit(int charcode, int fmt);
-int add(int a, int b);
-void pr_with_fmt(int charcode, int fmt);
-void print_to_98(int n);
-void pr_3_digit(int charcode, int fmt);
-void print_times_table(int n);
-void print_spaces(int n);
-void print_zero(void);
-
-#endif
->>>>>>> 0f9c44f97badc3c2c445bfe3792107f0fe804511
