5bcacceb2fa79d4be4b1b402092e9a798b66a4c6
MphoKut <90329295+MphoKut@users.noreply.github.com>
2022-02-14 11:30:31 +0200

    Pointers Arrays Strings

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,18 +1,17 @@
 #include "main.h"
 /**
- *print_rev - print strings in reverse
- *@s: characters
- *
- *Return: nothing
- */
+*print_re - print strings in reverse
+*@s: characters
+*/
 void print_rev(char *s)
 {
 int i;
 
-for (i = 0; s[i] != '\0'; i++)
+for(i = 0; s[i] != '\0'; i++)
+;
 while (i)
 {
-_putchar(s[i - 1]);
+  _putchar(s[i - 1]);
 i--;
 }
 _putchar('\n');
