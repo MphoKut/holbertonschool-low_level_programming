dbd17c85b084ed7ace0d257db3bca8bf5e8366e2
MphoKut <90329295+MphoKut@users.noreply.github.com>
2022-02-18 08:02:55 +0200

    More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,29 +1,33 @@
 #include "main.h"
 
 /**
- *_strncpy - copy strings
- *@dest: destination
- *@src: source
- *@n: integer
+ *_strncpy - Copy a string
+ * @dest: the destination value
+ * @src: The source value
+ * @n: The copy limit
  *
- *Return: destination
+ * Return: char value
  */
-
 char *_strncpy(char *dest, char *src, int n)
 {
+	int a = 0, b = 0;
 
-int j = 0;
+	while (src[b])
+	{
+		b++;
+	}
 
-for (j = 0; src[j] != '\0'; j++)
-{
-if (j < n)
-{
-dest[j] = src[j];
-}
-}
-for (; j < n; j++)
-{
-dest[j] = '\0';
-}
-return (dest);
+	while (a < n && src[a])
+	{
+		dest[a] = src[a];
+		a++;
+	}
+
+	while (a < n)
+	{
+		dest[a] = '\0';
+		a++;
+	}
+
+	return (dest);
 }
