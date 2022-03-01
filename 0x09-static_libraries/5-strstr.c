4867b5fc5e27eb5b93017e10bb86dbd3684dc5bf
MphoKut <90329295+MphoKut@users.noreply.github.com>
2022-03-01 09:19:30 +0200

    Static Libraries

--- 0x09-static_libraries/5-strstr.c
+++ 0x09-static_libraries/5-strstr.c
@@ -27,7 +27,6 @@ int _strncmp(char *s1, char *s2, unsigned int b)
 char *_strstr(char *haystack, char *needle)
 {
 	unsigned int len;
-	
 	/*Get length of needle for strncmp*/
 	len = 0;
 	while (needle[len] != '\0')
