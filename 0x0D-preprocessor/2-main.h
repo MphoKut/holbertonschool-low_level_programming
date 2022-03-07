f9e951e06ccf3b70221de718260fbd765f8fa68f
MphoKut <90329295+MphoKut@users.noreply.github.com>
2022-03-07 20:57:16 +0200

    Preprocessor

--- 0x0D-preprocessor/2-main.h
+++ 0x0D-preprocessor/2-main.h
@@ -1,1 +1,12 @@
-#include "main.h"
+#include <stdio.h>
+
+/**
+  * main - Prints the file name of the program
+  *
+  * Return: Always zero
+  */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
